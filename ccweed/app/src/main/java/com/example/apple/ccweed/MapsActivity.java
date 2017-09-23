090a76db8cfcf3f0ba0ce812546e5702aafe074a
Mobileheo <mobileheo@gmail.com>
2017-09-23 13:12:41 -0700

    Starting Point

--- ccweed/app/src/main/java/com/example/apple/ccweed/MapsActivity.java
+++ ccweed/app/src/main/java/com/example/apple/ccweed/MapsActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -22,6 +23,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+
     }
 
 
@@ -42,5 +45,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+//        int radiusInMeters = 5;
+//        int shadeColor
+//        CircleOptions circleOptions = new CircleOptions().center(sydney).radius(radiusInMeters).fillColor(shadeColor).strokeColor(strokeColor).strokeWidth(8);
+//        mMap.addCircle(circleOptions);
     }
+
 }
